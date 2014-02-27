4a0d25f8be1da6a7a26b89b5a02fb58ea23f28ad
Craig Gleckman <cgleckman@gmail.com>
2014-02-27 13:52:39 -0700

    completed??? pickle tester
    No output, even from print statements, but does detect error if
    purposely mess up line..
    
    Cant find any real difference from solution

--- pickle_test.py
+++ pickle_test.py
@@ -1,13 +1,24 @@
-import unittest
+import unittest, os, shutil
+import time
 from pickler import list_pickler, unpickler
 
 class PickleTests(unittest.TestCase):
 
     def setUp(self):
-        
+        self.pickle_list = [1,2,3,'a','b','c']
+        self.path = './pickle_temp'
+        os.makedirs(self.path)
+        self.file = os.path.join(self.path, 'list.pk1') #taken from solution - fixed file creation issue
+        print "Setup is complete..."
+
     def test_original_list_same_as_unpickled_list(self):
         list_pickler(self.file, self.pickle_list)
         unpickled_list = unpickler(self.file)
         self.assertEqual(self.pickle_list, unpickled_list)
+        print "Test complete..."
 
-    def tearDown(self):                   
+    def tearDown(self):
+                                 #copied from solution github
+                                 #didnt know how to do teardown
+        shutil.rmtree(self.path) #shutil.rmtree deletes a dir and all contents
+        
--- pickler.py
+++ pickler.py
@@ -2,5 +2,12 @@ import sys
 import pickle
 
 def list_pickler(path, pickle_list):
-    
+    f = open(path, 'wb')
+    pickle.dump(pickle_list, f)
+    f.close()
+
 def unpickler(path):
+    f = open(path, 'rb')
+    pickle_list = pickle.load(f)
+    f.close()
+    return pickle_list
